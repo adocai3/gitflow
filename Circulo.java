7b96e5a9fc33c2504b6856d9d795b06d9049c117
Adonis gra <adonixblur21@hotmail.com>
2025-02-21 11:30:23 +0100

    Agregar clase Circulo con métodos área y perímetro

---
+++ Circulo.java
@@ -0,0 +1,24 @@
+public class Circulo {
+    private double radio;
+    private static final double PI = Math.PI;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double calcularArea() {
+        return PI * radio * radio;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * PI * radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+}
